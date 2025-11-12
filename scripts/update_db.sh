99c8ab79cfebc35ec5bcb33cbf66841a9b208f66
Manuel Gusella <manuel.g@riskapp.it>
2025-11-12 15:44:43 +0100

    Modified the update to delete the db dir and then redo the entire db

--- scripts/update_db.sh
+++ scripts/update_db.sh
@@ -1,4 +1,4 @@
-set -euo pipefail
+set -e
 
 DB_DIR=${1:-/usr/local/overpass_db}
 PLANET_URL=${2:-https://download.geofabrik.de/europe/italy-latest.osm.pbf}
@@ -10,16 +10,12 @@ PLANET_GZ=italy-latest.osm.gz
 
 echo "Terminating dispatcher"
 ${OVERPASS_DIR}/bin/dispatcher --terminate
-rm -f ${DB_DIR}/osm3s_* || true
 
 echo "[update] downloading latest PBF"
-
-mkdir -p ${DB_DIR}
+rm -rf ${DB_DIR} && mkdir -p ${DB_DIR}
 cd ${DB_DIR}
 wget ${PLANET_URL}
 osmconvert ${DB_DIR}/${PLANET_PBF} | gzip -1 >${PLANET_GZ}
-
-#${OVERPASS_DIR}/bin/init_osm3s.sh ${DB_DIR}/${PLANET_PBF} ${DB_DIR} ${OVERPASS_DIR}
 
 gunzip <${DB_DIR}/${PLANET_GZ} | ${OVERPASS_DIR}/bin/update_database --db-dir=${DB_DIR}
 rm -f ${PLANET_PBF} ${PLANET_GZ}
@@ -33,6 +29,3 @@ chmod 666 ${DB_DIR}/osm3s_osm_base
 # non utilizzo fetch_osc_and_apply, visto che non applico cambiamenti globali, ho solo una mappa parziale
 #echo "115276" > ${DB_DIR}/replicate_id
 #${OVERPASS_DIR}/bin/fetch_osc_and_apply.sh "https://planet.openstreetmap.org/replication/hour/" &
-
-# Start query server
-#${OVERPASS_DIR}/bin/osm3s_query --db-dir=${DB_DIR} &
